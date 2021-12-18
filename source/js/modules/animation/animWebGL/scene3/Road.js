fd8569413dc47b0edc19219c1063c256216f11ad
natalia127 <ndorogan@bk.ru>
2021-12-18 12:38:43 +0300

    Использование кастомного материала ShaderMaterial для процедурных текстур «Коврика» и «Дороги»

--- source/js/modules/animation/animWebGL/scene3/Road.js
+++ source/js/modules/animation/animWebGL/scene3/Road.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import {getLathePointsForCircle, getLatheDegrees} from '../../utilsGeometry';
-
+import {RoadShaderMaterial} from "./roadShaderMaterial";
 
 export class Road extends THREE.Group {
   constructor() {
@@ -16,7 +16,7 @@ export class Road extends THREE.Group {
     const {start, length} = getLatheDegrees(0, 90);
 
     const base = new THREE.LatheBufferGeometry(points, 50, start, length);
-    this.baseMesh = new THREE.Mesh(base, new THREE.MeshBasicMaterial({color: 0x585F6D, flatShading: true, side: THREE.DoubleSide}));
+    this.baseMesh = new THREE.Mesh(base, new RoadShaderMaterial({color: 0x585F6D, flatShading: true, side: THREE.DoubleSide}));
 
     this.add(this.baseMesh);
   }
